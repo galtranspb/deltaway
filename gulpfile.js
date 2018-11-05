137c4df83d5c98df332e23ef5eebb60528041d95
Anatoly <AnatoliySokolov1981@gmail.com>
2018-11-06 00:05:06 +0300

    Mobile styles

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  plumber = require('gulp-plumber'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  server = require('browser-sync').create(),
+	svgmin = require('gulp-svgmin'),
+	cheerio = require('gulp-cheerio'),
+  replace = require('gulp-replace'),
+  sprite = require('gulp-svg-sprite');
+
+
+gulp.task('style', function () {
+  gulp.src('source/sass/style.scss')
+    .pipe(plumber())
+    .pipe(sass({
+      includePaths: require('node-normalize-scss').includePaths
+    }))
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest('source/css'))
+    .pipe(server.stream());
+});
+
+gulp.task('serve', ['style'], function () {
+  server.init({
+    server: 'source/',
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch('source/sass/**/*.{scss,sass}', ['style']);
+  gulp.watch('source/*.html').on('change', server.reload);
+});
+
+gulp.task('sprite', function () {
+	return gulp.src('source/img/*.svg')
+		.pipe(svgmin({
+			js2svg: {
+				pretty: true
+			}
+		}))
+		.pipe(cheerio({
+			run: function ($) {
+				$('[fill]').removeAttr('fill');
+				$('[stroke]').removeAttr('stroke');
+				$('[style]').removeAttr('style');
+			},
+			parserOptions: {xmlMode: true}
+		}))
+		.pipe(replace('&gt;', '>'))
+		.pipe(sprite({
+			mode: {
+				symbol: {
+					sprite: "../sprite.svg"
+				}
+			}
+		}))
+		.pipe(gulp.dest('source/img'));
+});
